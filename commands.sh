6172199b32f1e8892a9ca50a91862f8a0c06df67
MasterStrauss <blueplanet19x@gmail.com>
2021-04-27 17:06:08 +0100

    updated buildspec.yaml

--- commands.sh
+++ commands.sh
@@ -1,13 +1,11 @@
 # Create an S3 bucket
-aws s3 mb s3://sam-code-bucket-3
+aws s3 mb s3://sam-code-artifacts-3
 
 # Package Cloud Formation (sam package)
-aws cloudformation package --s3-bucket sam-code-bucket-3 --template-file template.yaml --output-template-file template-generated.yaml
+aws cloudformation package --s3-bucket sam-code-artifacts-3 --template-file template.yaml --output-template-file template-generated.yaml
 
 # Deploy
 aws cloudformation deploy --template-file template-generated.yaml --stack-name iteration3-sam --capabilities CAPABILITY_IAM
 
 
 aws cloudformation deploy --guided --template-file template-generated.yaml --stack-name iteration3-sam --capabilities CAPABILITY_IAM
-
-######
