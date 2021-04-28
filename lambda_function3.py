38157418a60e0005c68b1235901af1e6814e56ff
MasterStrauss <blueplanet19x@gmail.com>
2021-04-28 10:27:19 +0100

    commit 26

--- lambda_function3.py
+++ lambda_function3.py
@@ -3,13 +3,6 @@ import json
 import os
 import logging
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-
-print('Loading function')
-
-import json
-
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
